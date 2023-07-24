592d9c91fc0a511f8b4cd68d176976b56c688740
Mandillah123* <official1337h4x0r@gmail.com>
2023-07-24 20:45:32 +0300

    Node.js and Express.js - Full Course

---
+++ app.js
@@ -0,0 +1,11 @@
+//GLOBALS -NO WINDOW ! Because there's no browser
+
+//__dirname -path to current directory
+//__filename 
+//require -function to use modules
+//module  -info about current module
+//process -info about where program execution is happening
+
+
+
+
